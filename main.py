72b8741b4ea921bfefaff5f9ea4cf8b4a87d8dc2
Alexis Ballo <alexispballo@gmail.com>
2020-12-19 16:19:06 +0000

    print better

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import pandas as pd
 import numpy
 import csv
-import json
-
 
 newlist = []
+names = {}
+y = -1
 #print(newdata)
 #print(newdata[1])
 #equ = (hitts - homeruns)/(atbats - strikeouts - homeruns + sacrificefiles)
@@ -15,40 +14,38 @@ with open('sa.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
+        y = y+1
         newlist.append(row)
+        names[row[0]] = y
 
 #make sure list is generating correctly      
 print("list: ", newlist)
-
+def calculate(name):
+  x = names[name]
 #create dictionary from list [0] and [1]
-dict1 = {}
-dict1 = dict(zip(newlist[0],newlist[1]))
-for a,b in dict1.items():
-    try:
-        dict1[a] = float(b)
-    except:
-        try:
-            dict1[a] = int(b)
-        except:
-            print()
-#calcualte the thing from the keys
-equ1 = (dict1['Hits'] - dict1['Homeruns']) / (dict1['Atbats'] - dict1['Strikeouts'] - dict1['Homeruns'] + dict1['Sacrificeflies'])
-print(dict1['player'], "'s BIBIP for the" , int(dict1['year']), " season is:" , equ1)
+  dict1 = {}
+  dict1 = dict(zip(newlist[0],newlist[x]))
+  for a,b in dict1.items():
+      try:
+          dict1[a] = float(b)
+      except:
+          try:
+              dict1[a] = int(b)
+          except:
+              continue
+  #calcualte the thing from the keys
+  equ1 = (dict1['Hits'] - dict1['Homeruns']) / (dict1['Atbats'] - dict1['Strikeouts'] - dict1['Homeruns'] + dict1['Sacrificeflies'])
+  return dict1['player'], "'s BIBIP for the" , str(int(dict1['year'])), " season is: " , str(equ1)
+
+#need to convert the return value into a string to make it pretty
+def tupletostring(tup): 
+    str =  ''.join(tup) 
+    return str
+
+print(tupletostring(calculate('David Peralta')))
+print(tupletostring(calculate('steve yaegar')))
+print(tupletostring(calculate('Mark Grudzielanek')))
+print(tupletostring(calculate('Victor Martinez')))
 
 
-dict2 = {}
-
-#print("list: ", newlist)
-dict2 = {}
-dict2 = dict(zip(newlist[0],newlist[2]))
-for a,b in dict2.items():
-    try:
-        dict2[a] = float(b)
-    except:
-        try:
-            dict2[a] = int(b)
-        except:
-            print()
-#print(dict1)
-equ2 = (dict2['Hits'] - dict2['Homeruns']) / (dict2['Atbats'] - dict2['Strikeouts'] - dict2['Homeruns'] + dict2['Sacrificeflies'])
-print(dict2['player'], "'s BIBIP is:" , equ2)
+  
